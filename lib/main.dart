c48a4af672c32ae0bd28b4906bda97272368bf8b
Aravind <168095239+aravindr56@users.noreply.github.com>
2024-09-20 11:26:19 +0530

    aws

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,39 @@
-import 'package:amplify_core/amplify_core.dart';
-import 'package:awe_project/Screens/apply_leave_screen.dart';
-import 'package:awe_project/Screens/dashboard_screen.dart';
+// import 'package:amplify_core/amplify_core.dart';
+// import 'package:awe_project/Screens/apply_leave_screen.dart';
+// import 'package:awe_project/Screens/dashboard_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+// import 'package:get/get.dart';
+// import 'Screens/login_screen.dart';
+// import 'amplifyconfiguration.dart'; // Make sure this file exists and contains your Amplify configuration
+//
+// void main() async {
+//
+//
+//   runApp(MyApp());
+// }
+//
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       title: 'AWE Project',
+//       theme: ThemeData(),
+//       debugShowCheckedModeBanner: false,
+//       home: LoginScreen(),
+//     );
+//   }
+// }
+
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'Screens/login_screen.dart';
-import 'amplifyconfiguration.dart'; // Make sure this file exists and contains your Amplify configuration
+import 'amplifyconfiguration.dart'; // Generated during the initialization
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -43,7 +71,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
-
