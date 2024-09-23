420697bed93979e620da94f06402f00329b6538c
Cloud bees <cloudbeestech@gmail.com>
2024-09-23 15:02:37 +0530

    new changes

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -147,25 +147,24 @@ class _LoginScreenState extends State<LoginScreen> {
                 Align(
                   alignment: Alignment.topLeft,
                   child: SizedBox(
-                      width: 255,
-                      height: 260,
+                      width: 145,
+                      height: 150,
                       child: Image.asset('assets/images/curve.png')),
                 )
               ],
             ),
-            SizedBox(width: 10,),
             Column(
               children: [
-                SizedBox(height: 100,),
+                SizedBox(height: 140,),
                 SizedBox(
-                  width: 250,
-                  height: 100,
+                  width: 200,
+                  height: 90,
                   child: Image.asset('assets/images/logo (2).png'),
                 ),
-                SizedBox(height: 20,),
+                SizedBox(height: 30,),
                 Container(
-                  height: 320,
-                  width: 420,
+                  height: 240,
+                  width: 320,
                   decoration: BoxDecoration(
                       image: DecorationImage(
                         image: AssetImage('assets/images/login.png'),
@@ -174,13 +173,13 @@ class _LoginScreenState extends State<LoginScreen> {
                 )
               ],
             ),
-            SizedBox(width: 200,),
+            SizedBox(width: 60,),
             Card(
               elevation: 3,
               shadowColor: Colors.white,
               child: Container(
-                width: 420,
-                height: 440,
+                width: 340,
+                height: 400,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(10),
@@ -189,26 +188,26 @@ class _LoginScreenState extends State<LoginScreen> {
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(top: 80),
-                      child: Text("Login",style: TextStyle(fontSize: 32,fontFamily: 'Inter',decoration: TextDecoration.none,color: black),),
+                      child: Text("Login",style: TextStyle(fontSize: 22,fontFamily: 'Inter',decoration: TextDecoration.none,color: black),),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(right: 290,top: 10,bottom: 2),
-                      child: Text('User Id',style: TextStyle(fontFamily: 'Inter',fontSize: 16,color: black),),
+                      padding: const EdgeInsets.only(right: 190,top: 10,bottom: 2),
+                      child: Text('User Id',style: TextStyle(fontFamily: 'Inter',fontSize: 14,color: black),),
                     ),
-                    MyTextField(controller: userIdController, text: 'User ID', icon: Icons.person_outline),
+                    TabTextField(controller2: userIdController, text2: 'User ID', icon2: Icons.person_outline),
                     SizedBox(height: 10,),
                     Padding(
-                      padding: const EdgeInsets.only(right: 290,top: 10,bottom: 2),
+                      padding: const EdgeInsets.only(right: 170,top: 10,bottom: 2),
                       child: Text('Password',style: TextStyle(fontFamily: 'Inter',fontSize: 16,color: black),),
                     ),
-                    MyTextField(controller: passwordController, text: 'Password', icon: Icons.lock_outline),
-                    SizedBox(height: 30,),
+                    TabTextField(controller2: passwordController, text2: 'Password', icon2: Icons.lock_outline),
+                    SizedBox(height: 25,),
                     MaterialButton(
                       onPressed: (){
                         Navigator.push(context, MaterialPageRoute(builder: (context)=>DashBoardScreeen()));
                       },
-                      minWidth: 350,
-                      height: 40,
+                      minWidth: 250,
+                      height: 35,
                       color: yellow,
                       splashColor: yellow,
                       child: Text("Login",style: TextStyle(color: black,fontFamily: 'Open Sans',fontSize: 16),),
@@ -270,3 +269,47 @@ class MyTextField extends StatelessWidget {
   }
 }
 
+class TabTextField extends StatelessWidget {
+  final TextEditingController controller2;
+  final String text2;
+  final IconData icon2;
+  const TabTextField({super.key, required this.controller2, required this.text2, required this.icon2});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      shadowColor: Colors.white,
+      child: Container(
+        width: 260,
+        height: 35,
+        child: Material(
+          color: Colors.white,
+          child: TextField(
+            controller: controller2,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 10),  // Align the text vertically with the icon
+              prefixIcon: Icon(
+                icon2,
+                color: Colors.grey,
+                size: 21,  // Adjust icon size if needed
+              ),
+              hintText: text2,
+              hintStyle: TextStyle(
+                fontFamily: 'Inter',
+                fontSize: 15,
+                color: Colors.grey,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
