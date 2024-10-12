d8813d008480cf47878cdba01432fd0e1aaf2147
CloudBeesTechnology <CloudBeesTechnology@users.noreply.github.com>
2024-10-12 12:28:02 +0000

    Commit the artifacts.

--- artifacts/web/flutter_service_worker.js
+++ artifacts/web/flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "fc15326f2ce20d6c07de915783c37efd",
+"main.dart.js": "a8f0d08dc23c693b96fbd4b93fb9cfde",
 "assets/FontManifest.json": "866b9b20ab0e8c30ffe220d2a2d66abe",
 "assets/AssetManifest.bin": "4579192af421d53f0050402a73de5b4c",
-"assets/fonts/MaterialIcons-Regular.otf": "15f262fd07e3322a17644ad53c94cf02",
+"assets/fonts/MaterialIcons-Regular.otf": "1cfb2e1d4c861f45d7ec252948bb7d2d",
 "assets/packages/syncfusion_flutter_pdfviewer/assets/underline.png": "c94a4441e753e4744e2857f0c4359bf0",
 "assets/packages/syncfusion_flutter_pdfviewer/assets/fonts/RobotoMono-Regular.ttf": "5b04fdfec4c8c36e8ca574e40b7148bb",
 "assets/packages/syncfusion_flutter_pdfviewer/assets/squiggly.png": "c9602bfd4aa99590ca66ce212099885f",
@@ -56,7 +56,7 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "5e76247e7dfd8043a6e58c7708a3621f",
-"flutter_bootstrap.js": "98106741d6b9368552161a53cdbc32a9"};
+"flutter_bootstrap.js": "2e0c23dbd611f72741bd260d9e69f09f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
